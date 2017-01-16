84d9453cd4735cf2596ba088683608f705e3e9bf
wangpengbo <wpb504@126.com>
2017-01-16 23:04:42 +0800

    一些 UI 更改，activity 动画

--- app/src/main/java/com/apollo/discounthunter/ui/activity/GoodsDetailActivity.java
+++ app/src/main/java/com/apollo/discounthunter/ui/activity/GoodsDetailActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,6 +18,7 @@ import com.apollo.discounthunter.constants.Constants;
 import com.apollo.discounthunter.retrofit.model.Model;
 import com.apollo.discounthunter.utils.ImageLoaderUtils;
 import com.apollo.discounthunter.utils.IntentUtils;
+import com.apollo.discounthunter.utils.LogUtil;
 
 import butterknife.BindView;
 
@@ -157,5 +159,47 @@ public class GoodsDetailActivity extends BaseActivity {
                 finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float startX = 0;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startX = event.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float movingX = event.getX();
+                if (movingX - startX > 50) {//向右滑动50像素，判断为结束当前activity
+                    LogUtil.d(TAG, "结束");
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    float startX = 0;
+    float startY = 0;
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startX = ev.getRawX();//记录初始值
+                startY = ev.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float movingX = ev.getRawX();
+                float movingY = ev.getRawY();
+                if ((movingX - startX) > 200 && Math.abs(movingY - startY) < 100) {//向右滑动200像素，并且 y 方向滑动不超过100像素，判断为结束当前activity手势
+                    GoodsDetailActivity.this.finish();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
