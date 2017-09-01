af38690a07a525da53e54ae7084028933089c0ed
SuperApollo <wpb504@126.com>
2017-09-01 09:49:10 +0800

    启动页

--- app/src/main/java/com/apollo/discounthunter/ui/activity/WelcomeActivity.java
+++ app/src/main/java/com/apollo/discounthunter/ui/activity/WelcomeActivity.java
@@ -1,12 +1,12 @@
 package com.apollo.discounthunter.ui.activity;
 
-import android.os.Build;
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
-import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.apollo.discounthunter.R;
 import com.apollo.discounthunter.utils.IntentUtils;
@@ -15,36 +15,29 @@ import com.apollo.discounthunter.utils.IntentUtils;
  * Created by Apollo on 2017/8/31 17:01
  */
 
-public class WelcomeActivity extends BaseActivity {
+public class WelcomeActivity extends Activity {
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            IntentUtils.sendIntent(mContext, MainActivity.class);
+            IntentUtils.sendIntent(WelcomeActivity.this, MainActivity.class);
             WelcomeActivity.this.finish();
         }
     };
 
     @Override
-    protected int getLayoutId() {
-        return R.layout.activity_welcome;
-    }
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         /*set it to be no title*/
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-    @Override
-    protected int getMenuLayoutId() {
-        return -1;
-    }
-
-    @Override
-    protected void initView(View view) {
+        /*set it to be full screen*/
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_welcome);
+//        getActionBar().hide();
         mHandler.sendEmptyMessageDelayed(0, 3000);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getActionBar().hide();
-    }
 
     @Override
     protected void onDestroy() {
